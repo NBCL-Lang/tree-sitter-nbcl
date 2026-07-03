71531c064649df7d51d055de527789f8dc51695a
Byson94 <byson94@outlook.com>
2026-07-03 18:24:35 +0530

    feat: allow snake_idents to be SNAKE_IDENTS

--- grammar.js
+++ grammar.js
@@ -286,7 +286,7 @@ export default grammar({
       /#[^\n]*/,
       token(prec(2, seq('#-', repeat(choice(/[^-]/, seq('-', /[^#]/))), '-#'))),
     ),
-    snake_ident: $ => /[a-z_][a-zA-Z0-9_]*/,
+    snake_ident: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
     pascal_ident: $ => /[A-Z][a-zA-Z0-9_]*/
   }
 });
